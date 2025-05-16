fd843cdd4134583fe9f7f9cf64ad18ec23a386ad
daniel-kav <dankavatha01@gmail.com>
2025-05-16 08:57:18 +0300

    fix: correct HTML declaration and update base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   root: 'src',
-  base: '/ts-web/',
+  base: '/django-todo-develop/',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
